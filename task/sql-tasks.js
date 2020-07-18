f8edac01404cf8b4668e6389df88ee80eaeea38a
Ишанхонов Азизхон Юнусхон угли <m1801194@edu.misis.ru>
2020-07-18 13:08:42 +0300

    Revert "1-task"
    This reverts commit 6d87b316b76fb9c6969b087e1165a61430ea16f9.

--- task/sql-tasks.js
+++ task/sql-tasks.js
@@ -43,16 +43,7 @@ async function task_1_1(db) {
  *
  */
 async function task_1_2(db) {
-    let result = await db.query(`
-        SELECT 
-            CAST(OrderID AS CHAR) AS 'Order Id',
-            CAST(SUM(UnitPrice * Quantity) AS CHAR) AS 'Order Total Price',
-            (CAST((ROUND(SUM(Discount * Quantity) * 100 / SUM(UnitPrice * Quantity), 3)) AS CHAR) + 0) AS 'Total Order Discount, %'
-        FROM OrderDetails
-        GROUP BY OrderID
-        ORDER BY OrderID DESC;
-    `);
-    return result[0];
+    throw new Error("Not implemented");
 }
 
 /**
