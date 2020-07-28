7c138b1b3b9fc0f8a35d6408b8b07af68548b4bd
Ишанхонов Азизхон Юнусхон угли <m1801194@edu.misis.ru>
2020-07-29 01:04:04 +0300

    12-task

--- task/mongo-tasks.js
+++ task/mongo-tasks.js
@@ -453,7 +453,28 @@ async function task_1_11(db) {
  * Order products by price (asc) then by ProductName.
  */
 async function task_1_12(db) {
-    throw new Error("Not implemented");
+    let result = await db.collection('products').aggregate([
+        {
+          '$project': {
+            '_id': 0, 
+            'ProductName': 1, 
+            'UnitPrice': 1
+          }
+        }, {
+          '$sort': {
+            'UnitPrice': -1, 
+            'ProductName': 1
+          }
+        }, {
+          '$limit': 20
+        }, {
+          '$sort': {
+            'UnitPrice': 1, 
+            'ProductName': 1
+          }
+        }
+    ]).toArray();
+    return result;
 }
 
 /**
